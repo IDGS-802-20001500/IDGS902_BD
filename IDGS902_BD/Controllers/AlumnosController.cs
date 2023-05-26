cea39d9a3ff05fb89e142d050d900350fad0cd66
IDGS-802-20001500 <mauricioisraelfer@gmail.com>
2023-05-26 11:23:23 -0600

    Agregar archivos de proyecto.

---
+++ IDGS902_BD/Controllers/AlumnosController.cs
@@ -0,0 +1,25 @@
+﻿using IDGS902_BD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDGS902_BD.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+
+            using (AlumnosDBContext dbAlumno = new AlumnosDBContext())
+            {
+                //Select * from alumnos
+
+                return View(dbAlumno.Alumnos.ToList());
+            }
+            
+        }
+    }
+}
---
+++ IDGS902_BD/Models/AlumnosDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity
+using System.Linq;
+using System.Web;
+
+namespace IDGS902_BD.Models
+{
+    public class AlumnosDBContext:DbContext
+    {
+
+        public static string connection = "DefaultConnection";
+
+        public AlumnosDBContext() : base(connection) { }
+        
+        
+        public DbSet<Alumnos> Alumnos { get; set; }
+    }
+}
